b6c2896460019b93fb88bf91af1e12f6502dc792
Douglas R <dougir@hotmail.com>
2016-09-06 12:39:00 +1000

    Modified the index page slightly and added the connection.php file.

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+/* To include / connect: 
+include(connection.php);
+$conn = oci_connect($UName, $PWord, $DB)
+	or die("Error: Couldn't log in to database.");
+*/
+$UName = "s26913224";
+$PWord = "monash00";
+$DB = "fit2076";
+?>
