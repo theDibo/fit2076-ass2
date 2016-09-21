ebe4b9e4e5266127bdc2934ee661be86963a3ac9
Douglas R <dougir@hotmail.com>
2016-09-21 11:56:59 +1000

    Added a test data creation sql file.

---
+++ Documentation/test_data_schema.sql
@@ -0,0 +1,47 @@
+/* Test Data Creation File */
+
+/* Seller Data */
+INSERT INTO Seller VALUES (
+seller_seq.nextval, "Jack", "Morrison", "1 Soldier Lane", "Clayton", "VIC", "0123456789", null, "jmorrison@omail.com", "Y"
+);
+
+INSERT INTO Seller VALUES (
+seller_seq.nextval, "Hanzo", "Shimada", "4/23 Center Road", "Ormond", "VIC", null, "0234567891", "hshimada@omail.com", "Y"
+);
+
+INSERT INTO Seller VALUES (
+seller_seq.nextval, "Lucio", "Santos", "63 Jammin Street", "Bentleigh", "VIC", null, null, "lsantos@omail.com", "N"
+);
+
+INSERT INTO Seller VALUES (
+seller_seq.nextval, "Jesse", "McCree", "39 Cowboy Parade", "Braeside", "VIC", null, "0434123876", "jmcree@omail.com", "Y"
+);
+
+INSERT INTO Seller VALUES (
+seller_seq.nextval, "Gabriel", "Reyes", "420 Edgy Road", "Brighton", "VIC", "0398765432", null, "greyes@omail.com", "N"
+);
+
+/* Buyer Data */
+INSERT INTO Buyer VALUES (
+buyer_seq.nextval, "Lena", "Oxton", "6 Speedy Lane", "Ormond", "VIC", null, "0436454656", "loxton@omail.com", "Y"
+);
+
+INSERT INTO Buyer VALUES (
+buyer_seq.nextval, "Jamison", "Fawkes", "51 Rough Street", "Frankston", "VIC", "0387569887", null, "jfawkes@omail.com", "N"
+);
+
+INSERT INTO Buyer VALUES (
+buyer_seq.nextval, "Mei-Ling", "Zhou", "3/64 Frosty Drive", "North Melbourne", "VIC", null, "0498561324", "mzhou@omail.com", "Y"
+);
+
+INSERT INTO Buyer VALUES (
+buyer_seq.nextval, "Amelie", "Lacroix", "2 Scope Street", "Bentleigh", "VIC", null, "0498565678", "alacroix@omail.com", "Y"
+);
+
+INSERT INTO Buyer VALUES (
+buyer_seq.nextval, "Mako", "Rutledge", "51 Rough Street", "Frankston", "VIC", "0387569887", null, "mrutledge@omail.com", "Y"
+);
+
+/* X Data */
+/* X Data */
+/* X Data */
